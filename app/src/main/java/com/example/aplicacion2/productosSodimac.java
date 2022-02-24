7358397949bbd9204a05469a17a1bf1deac18c15
EliasRod1996 <elias96056@gmail.com>
2022-02-24 02:02:25 -0500

    V2.0

---
+++ app/src/main/java/com/example/aplicacion2/productosSodimac.java
@@ -0,0 +1,59 @@
+package com.example.aplicacion2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class productosSodimac extends AppCompatActivity {
+
+    Button btnBuscar,btnInsertar, btnActualizar, btnBorrar;
+    EditText txtProducto, txtPCosto, txtPxMayor,txtStock;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_productos_sodimac);
+
+        txtProducto = findViewById(R.id.txtProducto);
+        txtPCosto = findViewById(R.id.txtPCosto);
+        txtPxMayor = findViewById(R.id.txtPxMayor);
+        txtStock = findViewById(R.id.txtStock);
+
+        btnInsertar = findViewById(R.id.btnInsertar);
+        btnBuscar = findViewById(R.id.btnBuscar);
+        btnActualizar = findViewById(R.id.btnActualizar);
+        btnBorrar = findViewById(R.id.btnBorrar);
+
+        DBHelperProductos helper = new DBHelperProductos(this);
+
+
+        btnInsertar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String txproducto = txtProducto.getText().toString();
+                String txPcosto = txtPCosto.getText().toString();
+                String txPxMayor = txtPxMayor.getText().toString();
+                String txStock = txtStock.getText().toString();
+
+                if (TextUtils.isEmpty(txproducto) || TextUtils.isEmpty(txPcosto) || TextUtils.isEmpty(txPxMayor) || TextUtils.isEmpty(txStock))
+                    Toast.makeText(productosSodimac.this, "Debe llenar todos los campos", Toast.LENGTH_LONG).show();
+                else {
+
+
+                    Boolean inserta = helper.insertaDatosSodimac(txproducto, txPcosto, txPxMayor, txStock);
+                    if (inserta == true) {
+                        Toast.makeText(productosSodimac.this, "Datos insertados correctamente", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(productosSodimac.this, "Fallo al registrar producto", Toast.LENGTH_LONG).show();
+                    }}
+
+            }
+        });
+    }
+}
